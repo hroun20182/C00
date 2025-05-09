34de7c184a7f670f6e35074e56332bc2a0270c9e
hroun20182 <harounrachid0000@gmail.com>
2025-05-09 23:54:18 +0100

    c00

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-read -p "Entrer le nom du fichier : " FICHIER
-
-if [ -f "$FICHIER" ]; then
-  echo "Le fichier '$FICHIER' existe."
+echo "Entrer le nom du fichier : "
+read FICHIER
+if [ -f "\$FICHIER" ]; then
+echo "Le fichier '\$FICHIER' existe."
 else
-  echo "Le fichier '$FICHIER' n'existe pas."
-fi
+echo "Le fichier '\$FICHIER' n'existe pas."
+fi                                                                                                                                                                              verifie s'il y a pas de correction a faire

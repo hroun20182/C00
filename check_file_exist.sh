db3193046d7b8c0689e257abaeb16e86c2d177bf
hroun20182 <harounrachid0000@gmail.com>
2025-05-09 01:31:32 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,9 +1,11 @@
 !/bin/bash
-echo "Entrez le nom du fichier : " filename.
-read
 
-if [ -f "filename" ]; then
-    echo "Le fichier 'filename' existe."
+# Demande à l'utilisateur le nom du fichier
+read -p "Entrez le nom du fichier : " filename
+
+# Vérifie si le fichier existe
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
 else
-    echo "Le fichier 'filename' n'existe pas."
+    echo "Le fichier '$filename' n'existe pas."
 fi
